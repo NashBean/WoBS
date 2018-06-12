fa167a1f0aafd5f25f757774465b6878825048f9
Nash Bean <iBeanSoftware@gmail.com>
2018-06-12 14:05:02 -0400

    Update main.cpp

--- SoccerBotFactory/main.cpp
+++ SoccerBotFactory/main.cpp
@@ -7,11 +7,30 @@
 //
 
 #include <iostream>
+#include <vector>
+#include "iBS_SoccerBot.h"
 
+void GameLoop();
+    
 int main (int argc, const char * argv[])
 {
-
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    GameLoop();
     return 0;
 }
+
+void GameLoop()
+{
+    SoccerGameGrid field;
+    SoccerBotPlayer player[2];
+    SoccerBotCoatch team[2];
+    bool exit_flag = false;
+    //flip coin
+    do{
+        StartKickOff(int kicker);
+        if(Out_of_Bounds) StartSideThrough(int througher, ibsPosition location);
+        if(Corner_Kick) StartCornerKick(int kicker, ibsPosition location);
+        if(Score) {BumpScore(int team); if(Win) {ShowWinner(int winner); Stop_Game=true;} else StartKickOff(int kicker);}
+        if(Stop_Game) exit_flag = true;
+    }while(!exit_flag);
+};
